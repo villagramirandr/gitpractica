c6f20646563d08cec4b71300e5773bc2ed0d8409
Miranda <mirandadrvillagra@gmail.com>
2021-10-16 04:48:08 -0300

    confirmo confirmo

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const firstname = 'Miranda';
-const lastName = 'Villagra';
-/*estas son constantes de nombre y apellido - firstname: nombre - lastName (camel): apellido*/
+// const firstname = 'Miranda';
+// const lastName = 'Villagra';
+// /*estas son constantes de nombre y apellido - firstname: nombre - lastName (camelcase): apellido*/
 
-var age = 21;
-/*var: variable --> almacena un valor en este caso 21, no se pone en comillas*/
+// var age = 21;
+// /*var: variable --> almacena un valor en este caso 21, no se pone en comillas*/
 
-console.log (`${firstname}  ${lastName}  tiene  ${age} años`);
-/*console.log ejecuta en la terminal y muestra los valores y textos - 
-en la terminal hay que ejecutarlo como node index.js (o nombre del archivo js)*/
+
+//  console.log (`${firstname}  ${lastName}  tiene  ${age} años`);
+// /*console.log ejecuta en la terminal y muestra los valores y textos - 
+// en la terminal hay que ejecutarlo como node index.js (o nombre del archivo js)*/
 
 // age= age+1
-age++
-//age --> variable de edad - en este caso se suma + 1
-console.log (`${firstname}  ${lastName}  tiene  ${age} años`);
+// // age++
+// //age --> variable de edad - en este caso se suma + 1
+//  console.log (`${firstname}  ${lastName}  tiene  ${age} años`);
+
+// // const user = 'firstname: Angela, lastName: Davis, role: Professor';
+// //  console.log(user) 
+
+const persona = "Miranda Villagra. Edad:";
+var edad = 21;
+
+console.log (`${persona} ${edad}`);
+
+edad= edad+1;
+console.log (`Actualización edad: ${edad}`);
+
