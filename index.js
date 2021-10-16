bdba3eb8393f0ab6902f3f648ac27c7dc66dec04
Miranda <mirandadrvillagra@gmail.com>
2021-10-16 04:47:57 -0300

    confirmo (confirmar en "main")

---
+++ index.js
@@ -0,0 +1,15 @@
+const firstname = 'Miranda';
+const lastName = 'Villagra';
+/*estas son constantes de nombre y apellido - firstname: nombre - lastName (camel): apellido*/
+
+var age = 21;
+/*var: variable --> almacena un valor en este caso 21, no se pone en comillas*/
+
+console.log (`${firstname}  ${lastName}  tiene  ${age} años`);
+/*console.log ejecuta en la terminal y muestra los valores y textos - 
+en la terminal hay que ejecutarlo como node index.js (o nombre del archivo js)*/
+
+// age= age+1
+age++
+//age --> variable de edad - en este caso se suma + 1
+console.log (`${firstname}  ${lastName}  tiene  ${age} años`);
